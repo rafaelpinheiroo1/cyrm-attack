c6e15e5527f37f2fbc93acd6decfaf2fba8fb732
rafaelpinheiroo1 <rafaelpinheiro.380@gmail.com>
2024-05-07 15:14:41 -0300

    Up code

--- clear_cyrm.sh
+++ clear_cyrm.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 
-docker run --name containernet -it --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,source=$(pwd)/topology,target=/cyrm containernet-cyrm mn -c
+docker run --name containernet -it --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,source=$(pwd)/topology,target=/cyrm-attack containernet-cyrm mn -c
--- start_cyrm.sh
+++ start_cyrm.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 
-docker run --name containernet -it --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,source=$(pwd)/topology,target=/cyrm containernet-cyrm python3 /cyrm/scenario-1.py
+docker run --name containernet -it --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,source=$(pwd)/topology,target=/cyrm-attack containernet-cyrm python3 /cyrm-attack/corporate-attack-scenario.py
 
 
 
